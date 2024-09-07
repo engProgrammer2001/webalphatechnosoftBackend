5011ef4bb31c44e8abca82155a04cb8be8b3cbb7
engProgrammer2001 <askushwaha613@gmail.com>
2024-09-07 18:58:15 +0530

    cors error

--- server.js
+++ server.js
@@ -1,26 +1,25 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const cors = require('cors');
 const db = require('./src/config/db.js');
 const bodyParser = require("body-parser");
 const path = require("path");
-require ('dotenv').config()
+require('dotenv').config();
 
 app.use(bodyParser.json());
-app.use(cors());
 
+// Configure CORS
 app.use(cors({
-  origin: "http://localhost:3000/", // Replace with your client domain
-  credentials: true,
+  origin: 'http://localhost:3000', // Allow requests from your frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
+  credentials: true // Enable if you want to include cookies or authorization headers
 }));
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-
 app.get('/', function (req, res) {
-  res.send('Congratulation! Your server is running on port 5001!')
-})
-
+  res.send('Congratulations! Your server is running on port 5001!')
+});
 
 const userRouter = require('./src/routes/user.route.js');
 app.use('/user', userRouter);
@@ -28,9 +27,8 @@ app.use('/user', userRouter);
 const projectRouter = require('./src/routes/project.route.js');
 app.use('/project', projectRouter);
 
-
 const PORT = process.env.PORT || 5001;
 
-app.listen(PORT , () => {
-    console.log(`Server running on port ${PORT}`)
-}) 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+});
