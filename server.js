a5316b259e6df1ee49f5f41fbcea7463ecc7a7a8
engProgrammer2001 <askushwaha613@gmail.com>
2024-09-09 12:09:26 +0530

    cors

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
-const db = require('./src/config/db');
-const bodyParser = require('body-parser');
-const path = require('path');
+const cors = require('cors');
+const db = require('./src/config/db.js');
+const bodyParser = require("body-parser");
+const path = require("path");
 require('dotenv').config();
 
-// this is use to convert the json data to javascript object
-app.use(express.json());
-// Allow CORS from your frontend URL
+// Configure CORS to allow requests from localhost:3000
 app.use(cors({
-  origin: 'http://localhost:3000', // Frontend URL
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+  origin: 'http://localhost:3000', // Allow requests from this origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow these HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
+  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
 }));
 
 app.use(bodyParser.json());
-
-// Serve static files from the "uploads" directory
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.get('/', function (req, res) {
-  res.send('Congratulation! Your server is running on port 5001!');
+  res.send('Congratulation! Your server is running on port 5001!')
 });
 
 const userRouter = require('./src/routes/user.route.js');
